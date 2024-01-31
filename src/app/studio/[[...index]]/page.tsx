46d39ed3a62ebd7b86e2cdd9e4a55658dc242a04
Kellen Parker <kellenrparker@gmail.com>
2024-01-31 07:46:19 -0600

    more updates

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -8,15 +8,14 @@
  * You can learn more about the next-sanity package here:
  * https://github.com/sanity-io/next-sanity
  */
-
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../../sanity.config";
+import React from "react";
 
-
-export default function StudioPage() {
-  // Only allow in development
-  if (process.env.STAGE !== "development") {
-    return null;
-  }
+const StudioPage: React.FC = () => {
 	return <NextStudio config={config} />;
-}
+};
+
+export default StudioPage;
+
+export const dynamic = "force-dynamic";
